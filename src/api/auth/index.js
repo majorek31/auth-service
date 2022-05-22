77016358af963f9be06006bc874fe40f3b64e1d2
majorek31 <m4y0r.offical@gmail.com>
2022-05-22 15:26:18 +0200

    Import crypto library

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcrypt')
+const crypto = require('crypto')
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
