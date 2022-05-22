d30f35e650f8ae1977bf105cd55cc9bfd60529c4
majorek31 <m4y0r.offical@gmail.com>
2022-05-22 15:01:29 +0200

    Basic of /api/auth/login endpoint

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -1,6 +1,26 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcrypt')
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 
-
+router.get('/login', async (req, res) => {
+    if (!req.query.username || !req.query.password) 
+        return res.sendStatus(400)
+    try {
+        user = prisma.user.findFirst({
+            where: {
+                name: req.query.username
+            }
+        })
+        if (!user)
+            res.sendStatus(404)
+        result = await bcrypt.compare(req.query.password, user.password)
+        console.log(result)
+    } catch (err) {
+        console.error(err)
+        return res.sendStatus(500)
+    }
+})
 
 module.exports = router
