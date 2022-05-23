dd12d4120f5549025924203d2e6541c4bb9a1d99
majorek31 <m4y0r.offical@gmail.com>
2022-05-23 18:28:37 +0200

    Add /:post endpoint

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -26,4 +26,24 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/:post', async (req, res) => {
+    if (!req.headers.authorization || !parseInt(req.params.post))
+        return res.sendStatus(400)
+    if(!await utils.isSessionValid(req.headers.authorization))
+        return res.sendStatus(403)
+    try {
+        post = await prisma.post.findFirst({
+            where: {
+                id: parseInt(req.params.post)
+            }
+        })
+        if (!post)
+            return res.sendStatus(404)
+        return res.json(post)
+    } catch (err) {
+        console.error(err)
+        return res.sendStatus(500)
+    }
+})
+
 module.exports = router
