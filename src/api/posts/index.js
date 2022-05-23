111f0f9ae688136b3eec7d5a41bb38cd5a749f7e
majorek31 <m4y0r.offical@gmail.com>
2022-05-23 14:04:14 +0200

    Basic auth check

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const utils = require('../../utils');
 
-router.get('/', (req, res) => {
-    
+router.get('/', async (req, res) => {
+    if(!await utils.isSessionValid(req.headers.authorization))
+        return res.sendStatus(403)
+    return res.json({
+        message: 'You have been successfully authenticated!'
+    })
 })
 
 module.exports = router
