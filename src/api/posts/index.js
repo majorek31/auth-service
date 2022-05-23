d6351233638764717c234ce32f43859529fec1e1
majorek31 <m4y0r.offical@gmail.com>
2022-05-23 14:22:36 +0200

    Added /posts endpoint

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -1,13 +1,29 @@
 const express = require('express')
 const router = express.Router()
 const utils = require('../../utils');
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 
 router.get('/', async (req, res) => {
+    if (!req.headers.authorization)
+        return res.sendStatus(400)
     if(!await utils.isSessionValid(req.headers.authorization))
         return res.sendStatus(403)
-    return res.json({
-        message: 'You have been successfully authenticated!'
-    })
+    try {
+        posts = await prisma.post.findMany({
+            select: {
+                id: true,
+                title: true,
+                authorId: true,
+                tags: true,
+                createdAt: true,
+            }
+        })
+        return res.json(posts)
+    } catch (err) {
+        console.error(err)
+        return res.sendStatus(500)
+    }
 })
 
 module.exports = router
