32d5dda48fbe2152c19f0c580c02ed00c0ddee1e
majorek31 <m4y0r.offical@gmail.com>
2022-05-24 12:29:42 +0200

    Add usage of CORS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const config = require('../config.json');
 const bodyparser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
+app.use(cors())
 app.use(bodyparser.urlencoded({extended: true}));
 app.use('/api', require('./api'))
 
